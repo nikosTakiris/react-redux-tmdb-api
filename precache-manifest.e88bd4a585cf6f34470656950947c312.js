6ba13a9a9d48dfee97bd2c0d25261821459efc9c
Nikos Takiris <nik_takiris@yahoo.gr>
2019-05-02 04:20:34 +0300

    Updates

---
+++ precache-manifest.e88bd4a585cf6f34470656950947c312.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023e6e6dbf2e0e04e625222805b88514",
+    "url": "/react-redux-tmdb-api/index.html"
+  },
+  {
+    "revision": "1b58ee61ef14c608969f",
+    "url": "/react-redux-tmdb-api/static/css/main.9d323b3e.chunk.css"
+  },
+  {
+    "revision": "c7d8ebb05ae6a8a450c8",
+    "url": "/react-redux-tmdb-api/static/js/2.5d7eea98.chunk.js"
+  },
+  {
+    "revision": "1b58ee61ef14c608969f",
+    "url": "/react-redux-tmdb-api/static/js/main.2a4ba27a.chunk.js"
+  },
+  {
+    "revision": "a14ee7196994a7fe6b8b",
+    "url": "/react-redux-tmdb-api/static/js/runtime~main.848edb69.js"
+  }
+]);
